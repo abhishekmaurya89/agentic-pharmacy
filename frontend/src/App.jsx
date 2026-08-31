4b225f2dea2c93543f8621a6974b9189ca75699b
maurya72 <abhishekmaurya8936@gmail.com>
2026-08-31 21:31:25 +0530

    update page title and favicon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,18 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PatientDashboard from "./pages/PatientDashboard";
 import PharmacistDashboard from "./pages/PharmacistDashboard";
+
 function App() {
+  const defaultRoute = localStorage.getItem("access_token") ? "/patient" : "/login";
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/patient" element={<PatientDashboard />} />
         <Route path="/pharmacist" element={<PharmacistDashboard />} />
-        <Route path="/" element={<Navigate to="/patient" replace />} />
+        <Route path="/" element={<Navigate to={defaultRoute} replace />} />
       </Routes>
     </BrowserRouter>
   );
